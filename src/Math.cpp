a58a67198b87e3e002733c4ff6f112b6bea7d9c2
Eureka1257 <342680356@qq.com>
2025-11-17 17:47:39 +0800

    Matrix Caculation
    Finished matrix caculation methods

--- src/Math.cpp
+++ src/Math.cpp
@@ -1,7 +1,105 @@
 #include"Math.h"
+#include<string>
+#include"Base.h"
 
 namespace RGS
 {
+	float Dot(const Vector3& left, const Vector3& right)
+	{
+		return left.X * right.X + left.Y * right.Y + left.Z * right.Z;
+	}
+	/*Matrix4x4::Matrix4x4(const Vector4& v0, const Vector4& v1, const Vector4& v2, const Vector4& v3)
+	{
+		
+	}*/
+	Vector4 operator*(const Matrix4x4& mat, const Vector4& vec)
+	{
+		Vector4 res;
+		res.X = mat.m[0][0] * vec.X + mat.m[0][1] * vec.Y + mat.m[0][2] * vec.Z + mat.m[0][3] * vec.W;
+		res.Y = mat.m[1][0] * vec.X + mat.m[1][1] * vec.Y + mat.m[1][2] * vec.Z + mat.m[1][3] * vec.W;
+		res.Z = mat.m[2][0] * vec.X + mat.m[2][1] * vec.Y + mat.m[2][2] * vec.Z + mat.m[2][3] * vec.W;
+		res.W = mat.m[3][0] * vec.X + mat.m[3][1] * vec.Y + mat.m[3][2] * vec.Z + mat.m[3][3] * vec.W;
+		return {};
+	}
+	Matrix4x4 operator*(const Matrix4x4& left, const Matrix4x4& right)
+	{
+		Matrix4x4 res;
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				for (int k = 0; k < 4; k++)
+				{
+					res.m[i][j] += left.m[i][k] * right.m[k][j];
+				}				
+			}
+		}
+		return res;
+	}
+	Matrix4x4& operator*=(Matrix4x4& left, const Matrix4x4& right)
+	{
+		left = left * right;
+		return left;
+	}
+
+	Matrix4x4 Matrix4x4Identity()
+	{
+		return Matrix4x4({ 1,0,0,0 }, { 0,1,0,0 }, { 0,0,1,0 }, {0,0,0,1});
+	}
+	Matrix4x4 Matrix4x4Scale(const float sx, const float sy, const float sz)
+	{
+		Matrix4x4 m = Matrix4x4Identity();
+		ASSERT(sx != 0 && sy != 0 && sz != 0);
+		return Matrix4x4({ sx,0,0,0 }, { 0,sy,0,0 }, { 0,0,sz,0 }, { 0,0,0,1 });
+	}
+	Matrix4x4 Matrix4x4Translate(const float tx, const float ty, const float tz)
+	{
+		Matrix4x4 m = Matrix4x4Identity();
+		return Matrix4x4({ 1,0,0,tx }, { 0,1,0,ty }, { 0,0,1,tz }, { 0,0,0,1 });
+	}
+	Matrix4x4 Matrix4x4RotateX(const float angle)
+	{
+		float c = (float)cos(angle);
+		float s = (float)sin(angle);
+
+		return Matrix4x4({ 1,0,0,0 }, { 0,c, -s,0 }, { 0,s,c,0 }, { 0,0,0,1 });
+	}
+	Matrix4x4 Matrix4x4RotateY(const float angle)
+	{
+		float c = (float)cos(angle);
+		float s = (float)sin(angle);
+
+		return Matrix4x4({ c,0,s,0 }, { 0,1,0,0 }, { -s,0,c,0 }, { 0,0,0,1 });
+	}
+	Matrix4x4 Matrix4x4RotateZ(const float angle)
+	{
+		float c = (float)cos(angle);
+		float s = (float)sin(angle);
+
+		return Matrix4x4({ c, -s,0,0 }, { s,c,0,0 }, { 0,0,1,0 }, { 0,0,0,1 });
+	}
+	
+	Matrix4x4 Natrix4x4LookAt(const Vector3& xAxis, const Vector3& yAxis, const Vector3& zAxis, const Vector3& eye)
+	{
+		Matrix4x4 m = Matrix4x4Identity();
+		m.m[0][0] = xAxis.X;
+		m.m[0][1] = xAxis.Y;
+		m.m[0][2] = xAxis.Z;
+
+		m.m[1][0] = yAxis.X;
+		m.m[1][1] = yAxis.Y;
+		m.m[1][2] = yAxis.Z;
+
+		m.m[2][0] = zAxis.X;
+		m.m[2][1] = zAxis.Y;
+		m.m[2][2] = zAxis.Z;
+		//将摄像机平移到世界原点
+		m.m[0][3] = -Dot(xAxis, eye);
+		m.m[1][3] = -Dot(yAxis, eye);
+		m.m[2][3] = -Dot(zAxis, eye);
+
+		return m;
+	}
 	/// <summary>
 	/// 将float转换成UChar类型的函数
 	/// </summary>
--- src/Math.h
+++ src/Math.h
@@ -1,14 +1,24 @@
 #pragma once
+#include<string>
 
 namespace RGS 
 {
+	struct Vector2 
+	{
+		float X, Y;
+
+		constexpr Vector2() 
+			: X(0.0f), Y(0.0f) {}
+		constexpr Vector2(float x, float y) 
+			: X(x), Y(y) {}
+	};
 	struct Vector3
 	{
 		float X;
 		float Y;
 		float Z;
 
-		//learn constexpr: https://learn.microsoft.com/zh-cn/cpp/cpp/constexpr-cpp?view=msvc-170
+		
 		//这个关键字的作用是告诉编译器用硬编码的方式写入代码中 ， 
 		// 示例：constexpr Vector3 v（1.0f, 2.0f, 3.0f）;float x = v.X; 编译器会直接把1.0f写入x中，而不是在运行时创建一个Vector3对象再读取X成员
 		// 这样可以提高性能，减少运行时的开销
@@ -16,9 +26,164 @@ namespace RGS
 			: X(0.0f), Y(0.0f), Z(0.0f) {}
 		constexpr Vector3(float x, float y, float z)
 			: X(x), Y(y), Z(z) {}
+		/// <summary>
+		/// 将三维向量强转成二维向量
+		/// </summary>
+		operator Vector2() const
+		{
+			return{ X, Y };
+		}
+		
+	};
+	struct Vector4 {
+		float X;
+		float Y;
+		float Z;
+		float W;
+		constexpr Vector4()
+			: X(0.0f), Y(0.0f), Z(0.0f), W(0.0f) {}
+		constexpr Vector4(float x, float y, float z, float w)
+			: X(x), Y(y), Z(z), W(w) {}
+		constexpr Vector4(const Vector3& vec3, float w)
+			: X(vec3.X), Y(vec3.Y), Z(vec3.Z), W(w) {
+		}
+
+		operator Vector2() const
+		{
+			return{ X, Y };
+		}
+		operator Vector3() const
+		{
+			return{ X, Y, Z };
+		}
+
+		operator const std::string()const
+		{
+			std::string res;
+			res += "(";
+			res += std::to_string(X) + ", ";
+			res += std::to_string(Y) + ", ";
+			res += std::to_string(Z) + ", ";
+			res += std::to_string(W) + ")";
+			return res;
+		}
+	};
+	//这里按照行优先存储，第一列是m[0][0],m[1][0],m[2][0],m[3][0]
+	struct Matrix4x4
+	{
+		float m[4][4];
+		Matrix4x4()
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					m[i][j] = 0.0f;
+				}
+			}
+		}
+		/// <summary>
+		/// 按照列向量初始化矩阵
+		/// </summary>
+		/// <param name="v0"></param>
+		/// <param name="v1"></param>
+		/// <param name="v2"></param>
+		/// <param name="v3"></param>
+		Matrix4x4(const Vector4& v0, const Vector4& v1, const Vector4& v2, const Vector4& v3)
+		{
+			m[0][0] = v0.X; m[1][0] = v0.Y; m[2][0] = v0.Z; m[3][0] = v0.W;
+			m[0][1] = v1.X; m[1][1] = v1.Y; m[2][1] = v1.Z; m[3][1] = v1.W;
+			m[0][2] = v2.X; m[1][2] = v2.Y; m[2][2] = v2.Z; m[3][2] = v2.W;
+			m[0][3] = v3.X; m[1][3] = v3.Y; m[2][3] = v3.Z; m[3][3] = v3.W;
+		};
+
+		operator const std::string() const
+		{
+			std::string res;
+			res += "(";
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					res += std::to_string(m[i][j]);
+					res += (i == 3 && j == 3) ? ")" : ", ";
+				}
+			}
+			return res;
+		}
 	};
 
+	float Dot(const Vector3& left, const Vector3& right);
+	/// <summary>
+	/// 四维向量与矩阵的乘法
+	/// </summary>
+	/// <param name="mat"></param>
+	/// <param name="vec"></param>
+	/// <returns></returns>
+	Vector4 operator*(const Matrix4x4& mat, const Vector4& vec);
+	/// <summary>
+	/// 矩阵之间的乘法
+	/// </summary>
+	/// <param name="a"></param>
+	/// <param name="b"></param>
+	/// <returns></returns>
+	Matrix4x4 operator*(const Matrix4x4& left, const Matrix4x4& right);	
+	/// <summary>
+	/// 矩阵的*=运算符重载
+	/// </summary>
+	/// <param name="left"></param>
+	/// <param name="right"></param>
+	/// <returns></returns>
+	Matrix4x4& operator*=(Matrix4x4& left, const Matrix4x4& right);
 
+	/// <summary>
+	/// 单位矩阵
+	/// </summary>
+	/// <returns></returns>
+	Matrix4x4 Matrix4x4Identity();
+	/// <summary>
+	/// 缩放矩阵
+	/// </summary>
+	/// <param name="sx"></param>
+	/// <param name="sy"></param>
+	/// <param name="sz"></param>
+	/// <returns></returns>
+	Matrix4x4 Matrix4x4Scale(float sx, float sy, float sz);
+	/// <summary>
+	/// 平移矩阵
+	/// </summary>
+	/// <param name="tx"></param>
+	/// <param name="ty"></param>
+	/// <param name="tz"></param>
+	/// <returns></returns>
+	Matrix4x4 Matrix4x4Translate(float tx, float ty, float tz);
+	/// <summary>
+	/// 绕X轴旋转的矩阵
+	/// </summary>
+	/// <param name="angle"></param>
+	/// <returns></returns>
+	Matrix4x4 Matrix4x4RotateX(float angle);
+	/// <summary>
+	/// 绕Y轴旋转的矩阵
+	/// </summary>
+	/// <param name="angle"></param>
+	/// <returns></returns>
+	Matrix4x4 Matrix4x4RotateY(float angle);
+	/// <summary>
+	/// 绕Z轴旋转的矩阵
+	/// </summary>
+	/// <param name="angle"></param>
+	/// <returns></returns>
+	Matrix4x4 Matrix4x4RotateZ(float angle);
+	/// <summary>
+	/// 把摄像机旋转并移动到世界原点的矩阵
+	/// </summary>
+	/// <param name="xAxis"></param>
+	/// <param name="yAxis"></param>
+	/// <param name="zAxis"></param>
+	/// <param name="eye"></param>
+	/// <returns></returns>
+	Matrix4x4 Natrix4x4LookAt(const Vector3& xAxis, const Vector3& yAxis, const Vector3& zAxis, const Vector3& eye);
 	/// <summary>
 	/// 将浮点数转换为 unsigned char 值。
 	/// </summary>
